b6934b8c819cbbec587f3e4c93530e486e7d79d1
Diego Jacobs <diegojacobs9595@gmail.com>
2017-02-19 17:30:00 -0600

    Right click on canvas

--- MYPAINT.C
+++ MYPAINT.C
@@ -15,41 +15,15 @@
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
 
-#define v800x600x256             0x103
-#define NEW                      1
-#define SAVE                     2
-#define OPEN                     3
-#define PENCIL                   4
-#define LINE                     5
-#define RECTANGLE                6
-#define FILLEDRECTANGLE          7
-#define CIRCLE                   8
-#define FILLEDCIRCLE             9
-#define POLYGON                 10
-#define FILLEDPOLYGON           11
-#define ERASER                  12
-#define BUCKET                  13
-#define TEXT                    14
-#define EYEDROPPER              15
-#define COPY                    16
-#define CUT                     17
-#define PASTE                   18
-#define UNDO                    19
-#define PATTERN1                20
-#define PATTERN2                21
-#define SPRAY                   22
-
 void main ()
 {
     int selectedAction = PENCIL;
     int x, y, b, tempx, tempy, cont;
     int i, j;
     int x1, y1;
-    int* xval, yval;
     int rx, ry, xc, yc;
     char selectedColor1, selectedColor2;
     int thickness = 1;
-    Node *start;
     int a[20][2];
     
     FILE *fontFile;
@@ -78,134 +52,190 @@ void main ()
 
     canvas();
     toolBar();
+    showSelectedAction(PENCIL, 0);
+    showSelectedThickness(SMALL, 0);
 
     selectedColor1 = getPixel(25, 200);
     selectedColor2 = getPixel(75, 200);
+
     b = 0;
     do {
         refreshMouse(&x, &y, &b, &tempx, &tempy);
 
         //Click New
         if((x > 10 && x < 34) && (y > 8 && y < 41) && b==1){
+            hideMouse(x, y);
             canvas();
             toolBar();
+            showMouse(x, y);
         }
 
         //Click Save
         if((x > 10 && x < 42) && (y > 56 && y < 88) && b==1){
-
+            hideMouse(x, y);
+            showMouse(x, y);
         }
 
         //Click Open
         if((x > 42 && x < 75) && (y > 12 && y < 88) && b==1){
-
+            hideMouse(x, y);
+            showMouse(x, y);
         }
 
-        //Click Grossor 1
+        //Click Small Thickness
         if((x > 123 && x < 165) && (y > 24 && y < 33) && b==1){
-            thickness = 1;
+            hideMouse(x, y);
+            showSelectedThickness(SMALL, thickness);
+            thickness = SMALL;
+            showMouse(x, y);
         }
 
-        //Click Grossor 2
+        //Click Medium Thickness
         if((x > 123 && x < 165) && (y > 38 && y < 49) && b==1){
-            thickness = 2;
+            hideMouse(x, y);
+            showSelectedThickness(MEDIUM, thickness);
+            thickness = MEDIUM;
+            showMouse(x, y);
         }
 
-        //Click Grossor 3
+        //Click Big Thickness
         if((x > 123 && x < 165) && (y > 54 && y < 65) && b==1){
-            thickness = 3;
+            hideMouse(x, y);
+            showSelectedThickness(BIG, thickness);
+            thickness = BIG;
+            showMouse(x, y);
         }
 
         //Click Pencil
         if((x > 193 && x < 227) && (y > 7 && y < 44) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(PENCIL, selectedAction);
             selectedAction = PENCIL;
-            drawRectangle(191, 5, 229, 46, 0, 1);
+            showMouse(x, y);
         }
 
         //Click Line
         if((x > 195 && x < 222) && (y > 54 && y < 88) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(LINE, selectedAction);
             selectedAction = LINE;
-            drawRectangle(193, 52, 224, 90, 0, 1);
+            showMouse(x, y);
         }
 
         //Click Rectangle
         if((x > 241 && x < 273) && (y > 10 && y < 42) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(RECTANGLE, selectedAction);
             selectedAction = RECTANGLE;
-            drawRectangle(239, 8, 275, 44, 0, 1);
+            showMouse(x, y);
         }
 
         //Click Filled Rectangle
         if((x > 240 && x < 272) && (y > 56 && y < 88) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(FILLEDRECTANGLE, selectedAction);
             selectedAction = FILLEDRECTANGLE;
-            drawRectangle(238, 54, 274, 90, 0, 1);
+            showMouse(x, y);
         }
 
         //Click Circle
         if((x > 290 && x < 328) && (y > 8 && y < 46) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(CIRCLE, selectedAction);
             selectedAction = CIRCLE;
+            showMouse(x, y);
         }
 
         //Click Filled Circle
         if((x > 290 && x < 325) && (y > 56 && y < 88) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(FILLEDCIRCLE, selectedAction);
             selectedAction = FILLEDCIRCLE;
+            showMouse(x, y);
         }
 
         //Click Polygon
         if((x > 338 && x < 377) && (y > 8 && y < 47) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(POLYGON, selectedAction);
             selectedAction = POLYGON;
+            showMouse(x, y);
         }
 
         //Click Filled Polygon
         if((x > 336 && x < 377) && (y > 49 && y < 88) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(FILLEDPOLYGON, selectedAction);
             selectedAction = FILLEDPOLYGON;
+            showMouse(x, y);
         }
 
         //Click Eraser
         if((x > 425 && x < 462) && (y > 10 && y < 46) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(ERASER, selectedAction);
             selectedAction = ERASER;
-            drawRectangle(423, 8, 464, 48, 0, 1);
+            showMouse(x, y);
         }
 
         //Click Bucket
         if((x > 424 && x < 462) && (y > 53 && y < 90) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(BUCKET, selectedAction);
             selectedAction = BUCKET;
+            showMouse(x, y);
         }
 
         //Click Spray
         if((x > 392 && x < 413) && (y > 8 && y < 44) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(SPRAY, selectedAction);
             selectedAction = SPRAY;
-            drawRectangle(390, 6, 411, 46, 0, 1);
-        }
-
-        //Click Text
-        if((x > 467 && x < 502) && (y > 8 && y < 45) && b==1){
-            selectedAction = TEXT;
+            showMouse(x, y);
         }
 
         //Click EyeDropper
         if((x > 384 && x < 422) && (y > 55 && y < 92) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(EYEDROPPER, selectedAction);
             selectedAction = EYEDROPPER;
-            drawRectangle(382, 53, 424, 94, 0, 1);
+            showMouse(x, y);
+        }
+
+        //Click Text
+        if((x > 467 && x < 502) && (y > 8 && y < 45) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(TEXT, selectedAction);
+            selectedAction = TEXT;
+            showMouse(x, y);
         }
 
         //Click Copy
         if((x > 575 && x < 611) && (y > 8 && y < 43) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(COPY, selectedAction);
             selectedAction = COPY;
+            showMouse(x, y);
         }
 
         //Click Cut
         if((x > 578 && x < 608) && (y > 54 && y < 89) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(CUT, selectedAction);
             selectedAction = CUT;
+            showMouse(x, y);
         }
 
         //Click Paste
         if((x > 616 && x < 647) && (y > 9 && y < 46) && b==1){
+            hideMouse(x, y);
+            showSelectedAction(PASTE, selectedAction);
             selectedAction = PASTE;
+            showMouse(x, y);
         }
 
         //Click Undo
         if((x > 613 && x < 651) && (y > 55 && y < 86) && b==1){
-            selectedAction = UNDO;
             hideMouse(x, y);
             undo();
             showMouse(x, y);
@@ -213,128 +243,33 @@ void main ()
 
         //Click Pattern 1
         if((x > 701 && x < 745) && (y > 52 && y < 96) && b==1){
+            hideMouse(x, y);
             selectedAction = PATTERN1;
+            showMouse(x, y);
         }
 
         //Click Pattern 2
         if((x > 753 && x < 797) && (y > 52 && y < 96) && b==1){
+            hideMouse(x, y);
             selectedAction = PATTERN2;
+            showMouse(x, y);
         }
 
-        //Click en Palette
-        if((x > 23 && x < 41) && b==1){
-            if(y > 238 && y < 256)
-                selectedColor1 = getPixel(23, y);
-            if(y > 271 && y < 289)
-                selectedColor1 = getPixel(23, y);
-            if(y > 304 && y < 322)
-                selectedColor1 = getPixel(23, y);
-            if(y > 338 && y < 356)
-                selectedColor1 = getPixel(23, y);
-            if(y > 371 && y < 389)
-                selectedColor1 = getPixel(23, y);
-            if(y > 404 && y < 422)
-                selectedColor1 = getPixel(23, y);
-            if(y > 437 && y < 455)
-                selectedColor1 = getPixel(23, y);
-            if(y > 470 && y < 488)
-                selectedColor1 = getPixel(23, y);
-            if(y > 503 && y < 521)
-                selectedColor1 = getPixel(23, y);
-            if(y > 538 && y < 556)
-                selectedColor1 = getPixel(23, y);
-            if(y > 571 && y < 589)
-                selectedColor1 = getPixel(23, y);  
-
-            //Fill color 1 rectangle
-            drawFilledRectangle(5, 181, 44, 220, selectedColor1, selectedColor1, 1);
-        }
-
-        if((x > 57 && x < 75) && b==1){
-            if(y > 238 && y < 256)
-                selectedColor1 = getPixel(57, y);
-            if(y > 271 && y < 289)
-                selectedColor1 = getPixel(57, y);
-            if(y > 304 && y < 322)
-                selectedColor1 = getPixel(57, y);
-            if(y > 338 && y < 356)
-                selectedColor1 = getPixel(57, y);
-            if(y > 371 && y < 389)
-                selectedColor1 = getPixel(57, y);
-            if(y > 404 && y < 422)
-                selectedColor1 = getPixel(57, y);
-            if(y > 437 && y < 455)
-                selectedColor1 = getPixel(57, y);
-            if(y > 470 && y < 488)
-                selectedColor1 = getPixel(57, y);
-            if(y > 503 && y < 521)
-                selectedColor1 = getPixel(57, y);
-            if(y > 538 && y < 556)
-                selectedColor1 = getPixel(57, y);
-            if(y > 571 && y < 589)
-                selectedColor1 = getPixel(57, y);  
-
-            //Fill color 1 rectangle
-            drawFilledRectangle(5, 181, 44, 220, selectedColor1, selectedColor1, 1);
+        //Left Click in Palette
+        if(((x > 23 && x < 41) || (x > 57 && x < 75)) && b==1){
+            hideMouse(x, y);
+            selectedColor1 = getSelectedColor1(x - 1, y - 1);
+            showMouse(x, y);
         }
         
-        if((x > 23 && x < 41) && b==2){
-            if(y > 238 && y < 256)
-                selectedColor2 = getPixel(23, y);
-            if(y > 271 && y < 289)
-                selectedColor2 = getPixel(23, y);
-            if(y > 304 && y < 322)
-                selectedColor2 = getPixel(23, y);
-            if(y > 338 && y < 356)
-                selectedColor2 = getPixel(23, y);
-            if(y > 371 && y < 389)
-                selectedColor2 = getPixel(23, y);
-            if(y > 404 && y < 422)
-                selectedColor2 = getPixel(23, y);
-            if(y > 437 && y < 455)
-                selectedColor2 = getPixel(23, y);
-            if(y > 470 && y < 488)
-                selectedColor2 = getPixel(23, y);
-            if(y > 503 && y < 521)
-                selectedColor2 = getPixel(23, y);
-            if(y > 538 && y < 556)
-                selectedColor2 = getPixel(23, y);
-            if(y > 571 && y < 589)
-                selectedColor2 = getPixel(23, y);  
-
-            //Fill color 2 rectangle
-            drawFilledRectangle(57, 181, 96, 220, selectedColor2, selectedColor2, 1);
+        //Right Click in Palette
+        if(((x > 23 && x < 41) || (x > 57 && x < 75)) && b==2){
+            hideMouse(x, y);
+            selectedColor2 = getSelectedColor2(x - 1, y - 1);
+            showMouse(x, y);
         }
 
-        if((x > 57 && x < 75) && b==2){
-            if(y > 238 && y < 256)
-                selectedColor2 = getPixel(57, y);
-            if(y > 271 && y < 289)
-                selectedColor2 = getPixel(57, y);
-            if(y > 304 && y < 322)
-                selectedColor2 = getPixel(57, y);
-            if(y > 338 && y < 356)
-                selectedColor2 = getPixel(57, y);
-            if(y > 371 && y < 389)
-                selectedColor2 = getPixel(57, y);
-            if(y > 404 && y < 422)
-                selectedColor2 = getPixel(57, y);
-            if(y > 437 && y < 455)
-                selectedColor2 = getPixel(57, y);
-            if(y > 470 && y < 488)
-                selectedColor2 = getPixel(57, y);
-            if(y > 503 && y < 521)
-                selectedColor2 = getPixel(57, y);
-            if(y > 538 && y < 556)
-                selectedColor2 = getPixel(57, y);
-            if(y > 571 && y < 589)
-                selectedColor2 = getPixel(57, y);  
-
-            //Fill color 2 rectangle
-            drawFilledRectangle(57, 181, 96, 220, selectedColor2, selectedColor2, 1);
-        }
-
-        //Right click in canvas
+        //Left click in canvas
     	if((x > 100 && x < 800) && (y > 100 && y < 600) && b==1){
             switch(selectedAction){
                 case PENCIL:
@@ -417,8 +352,19 @@ void main ()
                 break;
 
                 case SPRAY:
+                    hideMouse(x, y);
+                    tempx = x;
+                    tempy = y;
                     saveUndo();
-                    spray(x, y, selectedColor1);
+                    while(b==1 && (x > 100 && x < 800) && (y > 100 && y < 600)){
+                        spray(x, y, selectedColor1);
+                        tempx = x; 
+                        tempy = y;
+                        getMouse(&x, &y, &b);
+                    }
+                    showMouse(x, y);
+                    tempx = x; 
+                    tempy = y;
                 break;
 
                 case EYEDROPPER:
@@ -555,6 +501,25 @@ void main ()
                 break;
             }
             
+        }
+
+        //Left click in canvas
+        if((x > 100 && x < 800) && (y > 100 && y < 600) && b==2){
+            switch(selectedAction){
+                case BUCKET:
+                    hideMouse(x, y);
+                    saveUndo();
+                    bucketFill(x, y, selectedColor2, 0);
+                    showMouse(x, y);
+                break;
+
+                case EYEDROPPER:
+                    if(b==1 && (x > 100 && x < 800) && (y > 100 && y < 600)){
+                        selectedColor2 = getPixel(x - 1, y - 1);
+                        drawFilledRectangle(57, 181, 96, 220, selectedColor2, selectedColor2, 1);
+                    }
+                break;
+            }
         }
 
     } while (!((x > 776 && x < 800) && (y > 0 && y < 24) && b == 1));
--- bar.h
+++ bar.h
@@ -1,36 +1,40 @@
-#define startXPalette            450
-#define startYPalette            26
-#define endXPalette              762
-#define endYPalette              63
-#define columns                  26
-#define size                     12
+#define SMALL                    1
+#define MEDIUM                   2
+#define BIG                      3
 
-void showPalette();
+#define NEW                      1
+#define SAVE                     2
+#define OPEN                     3
+#define PENCIL                   4
+#define LINE                     5
+#define RECTANGLE                6
+#define FILLEDRECTANGLE          7
+#define CIRCLE                   8
+#define FILLEDCIRCLE             9
+#define POLYGON                 10
+#define FILLEDPOLYGON           11
+#define ERASER                  12
+#define BUCKET                  13
+#define TEXT                    14
+#define EYEDROPPER              15
+#define COPY                    16
+#define CUT                     17
+#define PASTE                   18
+#define UNDO                    19
+#define PATTERN1                20
+#define PATTERN2                21
+#define SPRAY                   22
+
 void closeButton();
 void toolBar();
-char getSelectedColor(int x, int y);
 void canvas();
+char getSelectedColor1(int x, int y);
+char getSelectedColor2(int x, int y);
+void showSelectedThickness(int new, int old);
 
 void toolBar(){
     BITMAP bmp;
     loadImage(0, 0, "MyPaint/Images/toolbar.bmp", &bmp);
-}
-void showPalette(){
-    int i, j, k, l, color;
-    int startX = startXPalette;
-    int startY = startYPalette;
-    color = 152;
-
-    for(i = startX; color < 256; i = i + size, color++){
-        if(color % columns == 0)
-            i = startX;
-        
-        j = startY + ((color - 152) / columns) * size;
-
-        for(k = 0;k < size; j++, k++)
-            for (l = 0; l < size; l++)
-                putPixel(i + l, j, color); 
-    }
 }
 
 void canvas(){
@@ -38,8 +42,71 @@ void canvas(){
     loadImage(0, 100, "MyPaint/Images/canvas.bmp", &bmp);
 }
 
-char getSelectedColor(int x, int y){
-    return ((x - startXPalette) / size) + columns * ((y - startYPalette) / size) + 152;
+char getSelectedColor1(int x, int y){
+    char color;
+    if(y > 238 && y < 256)
+        color = getPixel(x, y);
+    if(y > 271 && y < 289)
+        color = getPixel(x, y);
+    if(y > 304 && y < 322)
+        color = getPixel(x, y);
+    if(y > 338 && y < 356)
+        color = getPixel(x, y);
+    if(y > 371 && y < 389)
+        color = getPixel(x, y);
+    if(y > 404 && y < 422)
+        color = getPixel(x, y);
+    if(y > 437 && y < 455)
+        color = getPixel(x, y);
+    if(y > 470 && y < 488)
+        color = getPixel(x, y);
+    if(y > 503 && y < 521)
+        color = getPixel(x, y);
+    if(y > 538 && y < 556)
+        color = getPixel(x, y);
+    if(y > 571 && y < 589)
+        color = getPixel(x, y);  
+
+    //Fill color 1 rectangle
+    drawFilledRectangle(5, 181, 44, 220, color, color, 1);
+
+    return color;
+}
+
+char getSelectedColor2(int x, int y){
+    char color;
+
+    if(y > 238 && y < 256)
+        color = getPixel(x, y);
+    if(y > 271 && y < 289)
+        color = getPixel(x, y);
+    if(y > 304 && y < 322)
+        color = getPixel(x, y);
+    if(y > 338 && y < 356)
+        color = getPixel(x, y);
+    if(y > 371 && y < 389)
+        color = getPixel(x, y);
+    if(y > 404 && y < 422)
+        color = getPixel(x, y);
+    if(y > 437 && y < 455)
+        color = getPixel(x, y);
+    if(y > 470 && y < 488)
+        color = getPixel(x, y);
+    if(y > 503 && y < 521)
+        color = getPixel(x, y);
+    if(y > 538 && y < 556)
+        color = getPixel(x, y);
+    if(y > 571 && y < 589)
+        color = getPixel(x, y);  
+
+    //Fill color 2 rectangle
+    drawFilledRectangle(57, 181, 96, 220, color, color, 1);
+
+    return color;
+}
+
+char getBarColor(){
+    return getPixel(790, 50);
 }
 
 void closeButton(){
@@ -49,5 +116,207 @@ void closeButton(){
         for(j=0;j<20;j++){
             putPixel(i, j, 12);
         }
+    }
+}
+
+void showSelectedThickness(int new, int old){
+    if (new == old){
+        return;
+    }
+
+    if (new == SMALL)
+    {
+        drawRectangle(120, 25, 167, 31, 0, 2);
+    }
+    
+    if (old == SMALL)
+    {
+        drawRectangle(120, 25, 167, 31, getBarColor(), 2);
+    }
+
+    if (new == MEDIUM)
+    {
+        drawRectangle(120, 39, 167, 48, 0, 2);
+    }
+    
+    if (old == MEDIUM)
+    {
+        drawRectangle(120, 39, 167, 48, getBarColor(), 2);
+    }
+
+    if (new == BIG)
+    {
+        drawRectangle(120, 54, 167, 64, 0, 2);
+    }
+    
+    if (old == BIG)
+    {
+        drawRectangle(120, 54, 167, 64, getBarColor(), 2);
+    }
+}
+
+void showSelectedAction(int new, int old){
+    if (new == old){
+        return;
+    }
+
+    if (new == PENCIL)
+    {
+        drawRectangle(191, 5, 229, 46, 0, MEDIUM);
+    }
+    
+    if (old == PENCIL)
+    {
+        drawRectangle(191, 5, 229, 46, getBarColor(), MEDIUM);
+    }
+
+    if (new == LINE)
+    {
+        drawRectangle(191, 52, 229, 90, 0, MEDIUM);
+    }
+    
+    if (old == LINE)
+    {
+        drawRectangle(191, 52, 229, 90, getBarColor(), MEDIUM);
+    }
+
+    if (new == RECTANGLE)
+    {
+        drawRectangle(236, 8, 275, 44, 0, MEDIUM);
+    }
+    
+    if (old == RECTANGLE)
+    {
+        drawRectangle(236, 8, 275, 44, getBarColor(), MEDIUM);
+    }
+
+    if (new == FILLEDRECTANGLE)
+    {
+        drawRectangle(236, 54, 275, 90, 0, MEDIUM);
+    }
+    
+    if (old == FILLEDRECTANGLE)
+    {
+        drawRectangle(236, 54, 275, 90, getBarColor(), MEDIUM);
+    }
+
+    if (new == CIRCLE)
+    {
+        drawRectangle(287, 6, 330, 48, 0, MEDIUM);
+    }
+    
+    if (old == CIRCLE)
+    {
+        drawRectangle(287, 6, 330, 48, getBarColor(), MEDIUM);
+    }
+
+    if (new == FILLEDCIRCLE)
+    {
+        drawRectangle(287, 54, 330, 90, 0, MEDIUM);
+    }
+    
+    if (old == FILLEDCIRCLE)
+    {
+        drawRectangle(287, 54, 330, 90, getBarColor(), MEDIUM);
+    }
+
+    if (new == POLYGON)
+    {
+        drawRectangle(334, 6, 379, 49, 0, MEDIUM);
+    }
+    
+    if (old == POLYGON)
+    {
+        drawRectangle(334, 6, 379, 49, getBarColor(), MEDIUM);
+    }
+
+    if (new == FILLEDPOLYGON)
+    {
+        drawRectangle(334, 47, 379, 90, 0, MEDIUM);
+    }
+    
+    if (old == FILLEDPOLYGON)
+    {
+        drawRectangle(334, 47, 379, 90, getBarColor(), MEDIUM);
+    }
+
+    if (new == ERASER)
+    {
+        drawRectangle(423, 8, 464, 48, 0, MEDIUM);
+    }
+    
+    if (old == ERASER)
+    {
+        drawRectangle(423, 8, 464, 48, getBarColor(), MEDIUM);
+    }
+
+    if (new == BUCKET)
+    {
+        drawRectangle(423, 51, 464, 92, 0, MEDIUM);
+    }
+    
+    if (old == BUCKET)
+    {
+        drawRectangle(423, 51, 464, 92, getBarColor(), MEDIUM);
+    }
+
+    if (new == SPRAY)
+    {
+        drawRectangle(382, 6, 424, 46, 0, MEDIUM);
+    }
+    
+    if (old == SPRAY)
+    {
+        drawRectangle(382, 6, 424, 46, getBarColor(), MEDIUM);
+    }
+
+    if (new == EYEDROPPER)
+    {
+        drawRectangle(382, 53, 424, 94, 0, MEDIUM);
+    }
+    
+    if (old == EYEDROPPER)
+    {
+        drawRectangle(382, 53, 424, 94, getBarColor(), MEDIUM);
+    }
+
+    if (new == TEXT)
+    {
+        drawRectangle(465, 10, 502, 52, 0, MEDIUM);
+    }
+    
+    if (old == TEXT)
+    {
+        drawRectangle(465, 10, 502, 52, getBarColor(), MEDIUM);
+    }
+
+    if (new == COPY)
+    {
+        drawRectangle(573, 6, 613, 45, 0, MEDIUM);
+    }
+    
+    if (old == COPY)
+    {
+        drawRectangle(573, 6, 613, 45, getBarColor(), MEDIUM);
+    }
+
+    if (new == CUT)
+    {
+        drawRectangle(573, 52, 613, 91, 0, MEDIUM);
+    }
+    
+    if (old == CUT)
+    {
+        drawRectangle(573, 52, 613, 91, getBarColor(), MEDIUM);
+    }
+
+    if (new == PASTE)
+    {
+        drawRectangle(614, 7, 649, 48, 0, MEDIUM);
+    }
+    
+    if (old == PASTE)
+    {
+        drawRectangle(614, 7, 649, 48, getBarColor(), MEDIUM);
     }
 }
