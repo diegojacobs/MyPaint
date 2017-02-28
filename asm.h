f1ff37b82aabef3c60940a22d6ccfd4cea2c4781
Diego Jacobs <diegojacobs9595@gmail.com>
2017-02-28 14:40:06 -0600

    Tab files

--- asm.h
+++ asm.h
@@ -16,19 +16,22 @@ char currentBank;
 
 int setVideoMode(int mode, long width, long height)
 {
-  resolutionWidth = width;
-  resolutionHeight = height;
-  nBanks = ((resolutionWidth*resolutionHeight)/65536L); 
-  asm{
-    MOV AX, 4F02H
-    MOV BX, [mode]
-    INT 10H                     
-    CMP AX, 004FH
+    resolutionWidth = width;
+    resolutionHeight = height;
+    nBanks = ((resolutionWidth*resolutionHeight)/65536L); 
+
+    asm{
+        MOV AX, 4F02H
+        MOV BX, [mode]
+        INT 10H                     
+        CMP AX, 004FH
     JNE Error
-  }
-  return(1);
-  Error:;
-  return(0);
+    }
+
+    return(1);
+    
+    Error:;
+    return(0);
 }
 
 void unsetVideoMode()
@@ -70,11 +73,13 @@ void putPixel(int x,int y, char color)
     // Offset = address - (bank * 0ffffh) = address - (bank * 2^16)
     // Left Shift (<<) = A<<B = A * 2^B
     off = address-(bank<<16);
+
     // Bank = address / 0ffffh = address / 2^16
     // Right Shift (>>) = A>>B = A / 2^B
     bank = address>>16;
 
-    if(bank != currentBank){
+    if(bank != currentBank)
+    {
         setBank(bank);
         currentBank = bank;
     }
@@ -120,32 +125,36 @@ void putThicknessPixel(int x, int y, char color, int thickness, int pattern)
 
 char getPixel(int x, int y)
 {
-  long address;
-  int off;  
-  char bank, color;
+    long address;
+    int off;  
+    char bank, color;
 
-  if(x>resolutionWidth || x<0 || y>resolutionHeight || y<0)
-    return;
+    if(x>resolutionWidth || x<0 || y>resolutionHeight || y<0)
+        return;
 
-  address = (resolutionWidth*y)+x;
-  // Offset = address - (bank * 0ffffh) = address - (bank * 2^16)
-  // Left Shift (<<) = A<<B = A * 2^B
-  off = address-(bank<<16);
-  // Bank = address / 0ffffh = address / 2^16
-  // Right Shift (>>) = A>>B = A / 2^B
-  bank = address>>16;
+    address = (resolutionWidth*y)+x;
+    
+    // Offset = address - (bank * 0ffffh) = address - (bank * 2^16)
+    // Left Shift (<<) = A<<B = A * 2^B
+    off = address-(bank<<16);
+    
+    // Bank = address / 0ffffh = address / 2^16
+    // Right Shift (>>) = A>>B = A / 2^B
+    bank = address>>16;
 
-  if(bank != currentBank){
-    setBank(bank);
-    currentBank = bank;
-  }
+    if(bank != currentBank)
+    {
+        setBank(bank);
+        currentBank = bank;
+    }
 
     asm{
-    MOV AX, 0A000H
-      MOV ES, AX
-      MOV DI, [off]
-      MOV AL, ES: [DI]
-      MOV [color], AL 
+        MOV AX, 0A000H
+        MOV ES, AX
+          MOV DI, [off]
+          MOV AL, ES: [DI]
+          MOV [color], AL 
     }
-  return color;
+
+    return color;
 }
--- text.h
+++ text.h
@@ -1,80 +1,87 @@
 int asciiI = 32;
-int fontW, fontH;			
+int fontWidth, fontHeight;			
 unsigned int * tempLine;
 
-/* Funciones */
 void setDimensions(int width, int height);
 int putChar(int x, int y, int nchar, char color, char masc, char font[][16*16]);
 void printText(int x, int y, char color, char font[][16*16]);
 
-//---------------------------------------------------------
-// Cambia las dimensiones de la fuente.
-//---------------------------------------------------------
+
 void setDimensions(int width, int height){
-	fontW = width;				//Ancho de la fuente.
-	fontH = height;				//Alto de la fuente.
+	fontWidth = width;				//Ancho de la fuente.
+	fontHeight = height;				//Alto de la fuente.
 }
 
 int putChar(int x, int y, int nchar, char color, char masc, char font[][16*16]){
-	int charOffset;
-	char w, h;
-	if(nchar>31 && nchar<91){
-		charOffset = nchar-asciiI;
-	}else if(nchar>96 && nchar<123){
-		charOffset = nchar-2*asciiI;
-	}else{
-		return 1;
-	}
-	
-	for(h=0; h<fontH; h++){
-		for(w=0; w<fontW; w++){
-			if(font[charOffset][h*fontW+w] != masc){
-				putPixel(x+w, y+h, color);
-			}
-		}
-	}
-	return 0;
+    int charOffset;
+    char w, h;
+    if(nchar>31 && nchar<91)
+    	charOffset = nchar-asciiI;
+    else 
+        if(nchar>96 && nchar<123)
+    	   charOffset = nchar-2*asciiI;
+        else
+    	   return 1;
+    
+
+    for(h = 0; h<fontHeight; h++)
+    	for(w = 0; w<fontWidth; w++)
+    		if(font[charOffset][h*fontWidth+w] != masc)
+    			putPixel(x+w, y+h, color);
+    
+    return 0;
 }
 
 
 void printText(int x, int y, char color, char font[][16*16]){
-  char texto;
-  int tempx, tempy;
-  int x1 = x;
-  int y1 = y;
-  unsigned char background[16][16];
-  delay(100);
-  texto = 0;
-  while(texto != 13){
+    char texto;
+    int tempx, tempy;
+    int x1 = x;
+    int y1 = y;
+    unsigned char background[16][16];
     delay(100);
-    if(kbhit()){
-      texto = getch();
-      if(!(x1 + 16 < 800)&& texto != 8){
-        if(y1 + 18 < 600){
-          x1 = x;
-          y1 += 18;
-        }else{
-          delay(100);
-          break;
+    texto = 0;
+    while(texto != 13)
+    {
+        delay(100);
+
+        if(kbhit())
+        {
+            texto = getch();
+            if(!(x1 + 16 < 800)&& texto != 8)
+            {
+                if(y1 + 18 < 600)
+                {
+                  x1 = x;
+                  y1 += 18;
+                }
+                else
+                {
+                  delay(100);
+                  break;
+                }
+            }
+
+            if(texto != 13 && texto != 8)
+            {
+                for(tempx = 0; tempx < 16; tempx++)
+                {
+                    for(tempy = 0; tempy < 16; tempy++)
+                        background[tempx][tempy] = getPixel(x1 + tempx, y1 + tempy);
+                }
+
+                putChar(x1, y1, texto, color, 0, font);
+                x1 += 17;
+            }
+            else 
+                if(texto == 8 && x1 != x)
+                {
+                    x1 += -17;
+                    for(tempx = 0; tempx < 16; tempx++)
+                        for(tempy = 0; tempy < 16; tempy++)
+                            putPixel(x1+tempx, y1+tempy, background[tempx][tempy]);
+                }
         }
-      }
-      if(texto != 13 && texto != 8){
-        for(tempx = 0; tempx < 16; tempx++){
-          for(tempy = 0; tempy < 16; tempy++){
-            background[tempx][tempy] = getPixel(x1 + tempx, y1 + tempy);
-          }
-        }
-        putChar(x1, y1, texto, color, 0, font);
-        x1+=17;
-      }else if(texto == 8 && x1 != x){
-        x1 += -17;
-        for(tempx = 0; tempx < 16; tempx++){
-          for(tempy = 0; tempy < 16; tempy++){
-            putPixel(x1+tempx, y1+tempy, background[tempx][tempy]);
-          }
-        }
-      }
+        delay(100);
     }
-    delay(100);
-  }
 }
